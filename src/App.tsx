5ad286197322a1799602140e917a2ec082cceef1
Kelsey Smith <pages.kelsey@gmail.com>
2022-04-07 13:13:06 -0400

    feat: header+sidebar components styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components/macro";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+const AppStyled = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-columns: 1fr 3fr;
+  background-color: #141414;
+  color: #ffffff;
+`;
+
+const HeaderContainer = styled.div`
+  grid-area: 1 / 1 / 2 / 3;
+`;
+
+const SidebarContainer = styled.div`
+  grid-area: 2 / 1 / 3 / 2;
+  padding: 2em;
+`;
+
+const ContentContainer = styled.div`
+  grid-area: 2 / 2 / 3 / 3;
+  padding: 2em;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyled>
+      <HeaderContainer>
+        <Header></Header>
+      </HeaderContainer>
+      <SidebarContainer>
+        <Sidebar></Sidebar>
+      </SidebarContainer>
+      <ContentContainer>
+        <div>Main page</div>
+      </ContentContainer>
+    </AppStyled>
   );
 }
 
